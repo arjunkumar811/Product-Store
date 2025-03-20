55ec74f63904f5ac97fb845f455326d2480f1c90
arjunkumar811 <kumararjun26401@gmail.com>
2025-03-21 01:08:35 +0530

    Added Controllers

---
+++ backend/controllers/product.controller.js
@@ -0,0 +1,55 @@
+import Product from "../models/product.model";
+
+
+export const getAllProducts = async (req, res) => {
+    try {
+        const products = await Product.find({});
+        res.status(200).json({ success: true, data: products });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+}
+
+
+export const deleteProduct = async (req, res) => {
+    const { id } = req.params;
+
+    try {    
+        const product = await Product.findByIdAndDelete(id);
+        res.status(200).json({ success: true, message: "Product deleted successfully" });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+}
+
+
+export const updateProduct =  async (req, res) => {
+    const { id } = req.params;
+    const product = req.body;
+
+    try {
+        const updatedProduct = await Product.findByIdAndUpdate(id, product, { new: true });
+        res.status(200).json({ success: true, data: updatedProduct });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+}
+
+
+export const postProduct = async (req, res) => {
+    const product = req.body;
+
+    if(!product.name || !product.price || !product.image) {     
+
+        return res.status(400).json({message: "All fields are required"});
+    }   
+
+    const newProduct = new Product(product);
+
+    try {
+        await newProduct.save();  
+        res.status(201).json({ success: true, data: newProduct });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+}
--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -1,61 +1,17 @@
 import express from "express";
-import mongoose from "mongoose";
-import Product from "../models/product.model";
+import { deleteProduct, getAllProducts, postProduct, updateProduct } from "../controllers/product.controller";
 
 const router = express.Router();    
 
 
-router.get("/", async (req, res) => {
-    try {
-        const products = await Product.find({});
-        res.status(200).json({ success: true, data: products });
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
-})
+router.get("/", getAllProducts);
 
 
-router.delete("/:id", async (req, res) => {
-    const { id } = req.params;
-
-    try {    
-        const product = await Product.findByIdAndDelete(id);
-        res.status(200).json({ success: true, message: "Product deleted successfully" });
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
-})
+router.delete("/:id", deleteProduct)
 
 
-router.post("/", async (req, res) => {
-    const product = req.body;
+router.post("/", postProduct)
 
-    if(!product.name || !product.price || !product.image) {     
-
-        return res.status(400).json({message: "All fields are required"});
-    }   
-
-    const newProduct = new Product(product);
-
-    try {
-        await newProduct.save();  
-        res.status(201).json({ success: true, data: newProduct });
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
-})
-
-router.put("/:id", async (req, res) => {
-    const { id } = req.params;
-    const product = req.body;
-
-    try {
-        const updatedProduct = await Product.findByIdAndUpdate(id, product, { new: true });
-        res.status(200).json({ success: true, data: updatedProduct });
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
-})
+router.put("/:id", updateProduct)
 
 
-export default Productrouter;
