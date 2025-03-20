e5b374e0fa1003d70d23e25a42cfccf6c7662fbe
arjunkumar811 <kumararjun26401@gmail.com>
2025-03-21 00:19:28 +0530

    Added Product endpoint

--- backend/server.js
+++ backend/server.js
@@ -2,14 +2,30 @@ import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 import { connectDB } from "./config/db.js";
+import Product from "./models/product.model.js";
 dotenv.config();
 
 
 const app = express();
+app.use(express.json());
 
 
-app.get("/products", (req, res) => {
-    res.send("Hello from the backend"); 
+app.post("/api/products", async (req, res) => {
+    const product = req.body;
+
+    if(!product.name || !product.price || !product.image) {     
+
+        return res.status(400).json({message: "All fields are required"});
+    }   
+
+    const newProduct = new Product(product);
+
+    try {
+        await newProduct.save();  
+        res.status(201).json({ success: true, data: newProduct });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
 })
 
 
