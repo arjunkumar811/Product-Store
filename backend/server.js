fb6c1d45f4da1d55805500bc2cb085b30765c685
arjunkumar811 <kumararjun26401@gmail.com>
2025-03-21 00:31:51 +0530

    Added Both get and delete end point

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,29 @@ const app = express();
 app.use(express.json());
 
 
+
+app.get("/api/products", async (req, res) => {
+    try {
+        const products = await Product.find({});
+        res.status(200).json({ success: true, data: products });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+})
+
+
+app.delete("/api/products/:id", async (req, res) => {
+    const { id } = req.params;
+
+    try {    
+        const product = await Product.findByIdAndDelete(id);
+        res.status(200).json({ success: true, message: "Product deleted successfully" });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+})
+
+
 app.post("/api/products", async (req, res) => {
     const product = req.body;
 
