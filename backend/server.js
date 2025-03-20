7575526f1d0a2e24cbce7ebc0af35239a0dbac17
arjunkumar811 <kumararjun26401@gmail.com>
2025-03-21 01:01:37 +0530

    Added routes

--- backend/server.js
+++ backend/server.js
@@ -1,68 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
-import mongoose from "mongoose";
 import { connectDB } from "./config/db.js";
-import Product from "./models/product.model.js";
+import Productrouter from "./routes/product.route.js";
 dotenv.config();
 
 
 const app = express();
 app.use(express.json());
 
-
-
-app.get("/api/products", async (req, res) => {
-    try {
-        const products = await Product.find({});
-        res.status(200).json({ success: true, data: products });
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
-})
-
-
-app.delete("/api/products/:id", async (req, res) => {
-    const { id } = req.params;
-
-    try {    
-        const product = await Product.findByIdAndDelete(id);
-        res.status(200).json({ success: true, message: "Product deleted successfully" });
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
-})
-
-
-app.post("/api/products", async (req, res) => {
-    const product = req.body;
-
-    if(!product.name || !product.price || !product.image) {     
-
-        return res.status(400).json({message: "All fields are required"});
-    }   
-
-    const newProduct = new Product(product);
-
-    try {
-        await newProduct.save();  
-        res.status(201).json({ success: true, data: newProduct });
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
-})
-
-app.put("/api/products/:id", async (req, res) => {
-    const { id } = req.params;
-    const product = req.body;
-
-    try {
-        const updatedProduct = await Product.findByIdAndUpdate(id, product, { new: true });
-        res.status(200).json({ success: true, data: updatedProduct });
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
-})
-
+app.use("/api/products", Productrouter);
 
 app.listen(3000, () => {
     connectDB();
