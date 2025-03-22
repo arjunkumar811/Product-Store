68e3319134ba0dd52e61367400b36803ff5d6f16
arjunkumar811 <kumararjun26401@gmail.com>
2025-03-22 13:56:28 +0530

    Added react-router dom

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,20 @@
-import { Button } from '@chakra-ui/react'
+import { Box } from "@chakra-ui/react"
+import { Route, Routes } from "react-router-dom"
+import Navbar from "./components/Navbar"
+import CreatePage from "./pages/CreatePage"
+import HomePage from "./pages/HomePage"
 
 function App() {
 
   return (
     <>
-      <Button>
-        hi there
-      </Button>
+  <Box minH={"100vh"}>
+    <Navbar />
+    <Routes>
+      <Route path="/" element={<HomePage />} />  
+      <Route path="/create" element={<CreatePage />} />
+    </Routes>
+    </Box>      
     </>
   )
 }
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,12 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { ChakraProvider } from '@chakra-ui/react'
 import App from './App'
+import { BrowserRouter as BrowserRoute } from 'react-router-dom'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+<BrowserRoute>
     <ChakraProvider>
     <App />
     </ChakraProvider>
-    
+    </BrowserRoute>
   </StrictMode>,
 )
